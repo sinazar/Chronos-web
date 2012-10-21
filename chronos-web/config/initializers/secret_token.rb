71fac957e63dec3a63e0a239761607ef7a58c800
Sina Zargaran <sina.zargaran@hotmail.com>
2012-10-20 23:39:17 -0400

    initial commit

---
+++ chronos-web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChronosWeb::Application.config.secret_token = 'b1910e5989b89af73eb3af5176ca1e06257ad002a1327bb6ee296fda2792dbf7021a68c019270f34b0f1d3d94768dc27c07c5c7c63f3f7c140527b13d97fe754'
